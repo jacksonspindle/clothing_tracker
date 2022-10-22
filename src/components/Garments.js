52532b61a403ab6e3887a685c767241d4121fe43
Jackson Spindle <jacksonmilesspindle@gmail.com>
2022-10-21 19:32:41 -0600

    started working on filter selections on Garments page

--- src/components/Garments.js
+++ src/components/Garments.js
@@ -1,16 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 const Garments = () => {
     const { garments } = useSelector(state => state)
     const dispatch = useDispatch()
-    
+
+    const [filterState, setFilterState] = useState("")
+    const [filterChoice, setFilterChoice] = useState('shirt')
+
+    console.log(filterState)
     return(
         <div className='garment-container'>
+            <div>
+            <ul>Garment Type:
+                <li onClick={() => {
+                    setFilterState("garment => garment.garmentType === 'shirt'")
+                    setFilterChoice('shirt')
+                }}>Shirts</li>
+                <li onClick={() => {
+                    setFilterState("garment => garment.garmentType === 'pant'")
+                    setFilterChoice('pant')
+                }}>Pants</li>
+                <li onClick={() => {
+                    setFilterState("garment => garment.garmentType === 'jacket'")
+                    setFilterChoice('jacket')
+                }}>Jackets</li>
+                <li onClick={() => {
+                    setFilterState("garment => garment.garmentType === 'hoodie'")
+                    setFilterChoice('hoodie')
+                }}>Hoodies</li>
+                <li onClick={() => {
+                    setFilterState("garment => garment.garmentType === 'sweater'")
+                    setFilterChoice('sweater')
+                }}>Sweaters</li>
+                <li onClick={() => {
+                    setFilterState("garment => garment.garmentType === 'shoe'")
+                    setFilterChoice('shoe')
+                }}>Shoes</li>
+            </ul>
+            <ul>Size:
+                <li onClick={() => {
+                    setFilterState("garment => garment.size === 'S'")
+                    setFilterChoice('S')
+                }}>Small</li>
+                <li>Medium</li>
+                <li>Large</li>
+                <li>X Large</li>
+            </ul>
+            <ul>Price:
+                <li>$ 0-100</li>
+                <li>$ 100-300</li>
+                <li>$ 300-800</li>
+                <li>$ 800 +</li>
+            </ul>
+        </div>
             <ul className='garment'>
                 {
-                    garments.map(garment => {
+                    garments.filter((filterState === '') ? (garment => garment.id !== null) : (garment => garment.size === filterChoice)).map(garment => {
                         return(
                             <div   key={garment.id}>
                                <Link className='' to={`/garments/${garment.id}`}>
---
+++ src/components/SearchFilters.js
@@ -0,0 +1,32 @@
+// import React from "react";
+
+// const SearchFilters = (props) => {
+//     const [filterState, setFilterState] = useState('')
+//     return(
+//         <div>
+//             <ul>Garment Type:
+//                 <li>Shirts</li>
+//                 <li>Pants</li>
+//                 <li>Jackets</li>
+//                 <li>Hoodies</li>
+//                 <li>Sweaters</li>
+//                 <li>Shoes</li>
+//             </ul>
+//             <ul>Size:
+//                 <li>Small</li>
+//                 <li>Medium</li>
+//                 <li>Large</li>
+//                 <li>X Large</li>
+//             </ul>
+//             <ul>Price:
+//                 <li>$ 0-100</li>
+//                 <li>$ 100-300</li>
+//                 <li>$ 300-800</li>
+//                 <li>$ 800 +</li>
+//             </ul>
+//         </div>
+//     )
+// }
+
+// export default SearchFilters
+
